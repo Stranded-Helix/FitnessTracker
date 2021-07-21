34985e54fc3cd0f06a51713235258213f1d3f7c2
Garrett Fite <36345799+Stranded-Helix@users.noreply.github.com>
2021-07-21 13:36:43 -0500

    set up other workout routes

--- routes/api/workoutRoutes.js
+++ routes/api/workoutRoutes.js
@@ -10,3 +10,41 @@ router.get("/", async (req, res) => {
     }
     
 });
+
+router.put('/:id', async (req , res) => {
+    try {
+        const workout = await db.Workout.findOneAndUpdate(
+            { _id: req.params.id },{ $push:  { exercises: req.body } }, { runValidators: true }
+        )
+
+        res.status(200).json(workout)
+    }   catch (err) {
+        res.status(400).json(err)
+    } 
+})
+
+router.post("/", async (req, res) => {
+    try {
+        //TODO: fix post to match model
+        const newWorkout = await db.Workout.create(req.body);
+        res.json(newWorkout);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get("/range", async (req, res) => {
+    try{
+        const workoutData = await db.Workout.aggregate([{
+            $addFields: {totalDuration: {$sum: '$exercises.duration'}}
+        }]);
+        if(workoutData.length > 7) {
+            workoutData.splice(0, workoutData.length - 7);
+        }
+        res.json(workoutData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
