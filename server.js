524c538a3719e280c4169b31ed3d9c3fd6c30f85
Garrett Fite <36345799+Stranded-Helix@users.noreply.github.com>
2021-07-21 13:14:05 -0500

    added server file

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
